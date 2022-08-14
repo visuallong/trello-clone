1a9399423efcd5a417b68d0724eb7b24c3dc3ce4
Nguyen <babyfacedeveloper@gmail.com>
2022-08-14 00:26:28 -0400

    create app setting

--- api/settings.py
+++ api/settings.py
@@ -1,8 +1,23 @@
+from functools import lru_cache
 from pydantic import BaseSettings
+
+
+@lru_cache()
+def get_app_settings():
+    """
+    Get app settings from Environment variables
+    check more at https://fastapi.tiangolo.com/advanced/settings/#creating-the-settings-only-once-with-lru_cache
+    """
+    return AppSettings()
 
 
 class AppSettings(BaseSettings):
     env: str
+    secret_key: str
+    jwt_algorithm: str
+    app_host: str
+    app_port: int
+    access_token_expire_minutes: int
 
     class Config:
         env_file: str = ".env"
