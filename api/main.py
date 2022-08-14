fef5f305e80ea38843473a284529e3830a9f5096
Nguyen <babyfacedeveloper@gmail.com>
2022-08-13 22:46:10 -0400

    test gh actions format python code

--- api/main.py
+++ api/main.py
@@ -1,14 +1,11 @@
-from fastapi import FastAPI
-from functools import lru_cache
-from settings import AppSettings
+from fastapi import FastAPI, HTTPException, status, Depends
+from dtos import LoginModel, RegisterModel
+from utils import create_access_token, get_current_user, get_user, verfiy_password
+from settings import get_app_settings
 import uvicorn
 
 app = FastAPI()
-
-
-@lru_cache()
-def get_settings():
-    return AppSettings()
+app_settings = get_app_settings()
 
 
 @app.get("/")
@@ -16,10 +13,39 @@ async def index():
     return {"message": "Hello World"}
 
 
-if __name__ == '__main__':
-    if get_settings().env == "dev":
-        uvicorn.run("main:app", host="localhost", port=3000,
-                    reload=True, debug=True, workers=3)
-    elif get_settings().env == "prod":
+@app.post("/api/auth/signup")
+async def signup(register_model: RegisterModel):
+    pass
+
+
+@app.post("/api/auth/login")
+async def login(login_model: LoginModel):
+    user=get_user(login_model.username)
+
+    if not user or (
+        user and not verfiy_password(login_model.password, user["password"])
+    ):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="username or password incorrect",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    return {"token": create_access_token(user), "token_type": "bearer"}
+
+
+@app.get("/api/boards")
+async def get_user_board(current_user=Depends(get_current_user)):
+    return{"user board": "TODO"}
+
+
+if __name__ == "__main__":
+    if app_settings.env == "dev":
+        uvicorn.run(       "main:app",      host=app_settings.app_host,           port=app_settings.app_port,
+            reload=True,
+            debug=True,
+            workers=3,
+        )
+    elif app_settings.env == "prod":
         # TODO
         print("app running at *:3000")
